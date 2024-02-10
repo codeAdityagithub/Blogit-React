f337a52f2aee59f90d4de695762c5a97587abf22
Akash Yadav <akashyadav3634@gmail.com>
2024-02-10 11:05:20 +0530

    testing from Akash

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 app.listen(8000, () => {
-    console.log("server is running");
+    console.log("server is running at http://localhost:8000");
 });
